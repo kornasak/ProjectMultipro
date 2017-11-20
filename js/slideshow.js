154064057f183e4814fe51e9aff45461a7ac04b4
KornDva <kornasak@outlook.com>
2017-11-21 02:34:55 +0700

    test

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,54 +1,30 @@
-(function($) {
-    $.fn.slideShow = function(settings) {
+$(document).ready(function(){
+    $('.slide').first().addClass('active');
+    $('.slide').hide();   
+    $('.active').show();
 
-        settings = $.extend({ message: '<p class="clickMe">Click slides to progress through presentation</p>' }, settings);
-        var list = $(this);
-        var listItems = list.children('.slide');
-        var listHeight = $(listItems).css('height');
-        var listWidth = $(listItems).css('width');
-
-        $(listItems).addClass('go');
-        $(listItems).last().addClass('stop').removeClass('go');
-
-        list.append(listItems.get().reverse());
-
-        $(listItems).first().append('<p class="clickMe">' + settings.message + '</p>');
-
-        $('.clickMe').css({
-            'font-size': '12px',
-            'line-height': '12px',
-            'font-weight': 'normal',
-            'text-align': 'center',
-            'position': 'absolute',
-            'bottom': '0px',
-            'background': 'rgba(255,255,255,.45)',
-            'padding': '10px 0 20px 0',
-            'margin': '0px',
-            'width': '100%'
-        });
-
-        $(list).css({
-            'height': listHeight,
-            'overflow': 'hidden',
-            'position': 'relative'
-        });
-        $('.slide').css({
-            'width': listWidth,
-            'position': 'absolute',
-            'top': '0px',
-            'left': '0px',
-            'cursor': 'pointer'
-        });
-
-        $('.go').on('click', function() {
-            $(this).fadeOut(250);
-        });
-
-        $('.stop').on('click', function() {
-            $('.go').fadeIn(250);
-        });
-
-    };
-})(jQuery);
-
-$('.taco').slideShow();
+    $('#button-next').click(function(){
+        $('.active').removeClass('active').addClass('oldActive');    
+        if ( $('.oldActive').is(':last-child')) {
+            $('.slide').first().addClass('active');
+        }
+        else{
+            $('.oldActive').next().addClass('active');
+        }
+        $('.oldActive').removeClass('oldActive');
+        $('.slide').fadeOut();
+        $('.active').fadeIn();
+    });
+    $('#button-previous').click(function(){
+        $('.active').removeClass('active').addClass('oldActive');    
+        if ( $('.oldActive').is(':first-child')) {
+            $('.slide').last().addClass('active');
+        }
+        else{
+            $('.oldActive').prev().addClass('active');
+        }
+        $('.oldActive').removeClass('oldActive');
+        $('.slide').fadeOut();
+        $('.active').fadeIn();
+    });
+});
